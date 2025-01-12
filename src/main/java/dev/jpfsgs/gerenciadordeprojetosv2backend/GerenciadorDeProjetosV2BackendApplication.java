6a8202c7f59065f5fae3219017d7bd57bb2734ef
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-01-11 23:16:46 -0300

    fix: swagger https schema

--- src/main/java/dev/jpfsgs/gerenciadordeprojetosv2backend/GerenciadorDeProjetosV2BackendApplication.java
+++ src/main/java/dev/jpfsgs/gerenciadordeprojetosv2backend/GerenciadorDeProjetosV2BackendApplication.java
@@ -1,9 +1,16 @@
 package dev.jpfsgs.gerenciadordeprojetosv2backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 @SpringBootApplication
 @EnableCaching
 public class GerenciadorDeProjetosV2BackendApplication {
